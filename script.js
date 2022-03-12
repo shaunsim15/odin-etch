a98fbe79b90a43c4622855705efe6e55bcc21b92
shaunsim <shaunsim25@gmail.com>
2022-03-12 22:32:43 +0800

    Add button color changes and fix background whitespace

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-const slider = document.querySelector(".slider");
-const bullet = document.querySelector(".bullet");
-const sketchContainer = document.querySelector(".sketch-container");
-const black = document.querySelector(".black");
-const gradual = document.querySelector(".gradual");
-const rainbow = document.querySelector(".rainbow");
-const erase = document.querySelector(".erase");
-const reset = document.querySelector(".reset");
-let colorStatus = "black";
-
-// Sets the bullet value to the initial HTML slider value (35) when the page loads
-bullet.textContent = `${slider.value} x ${slider.value}`;
-
-// Sets the bullet value to the live slider value when user input is registered
-slider.addEventListener('input',showSliderValue)
-
-
+// Creates a text bullet over the slider which shows the grid dimensions
 function showSliderValue() {
     bullet.textContent = `${slider.value} x ${slider.value}`;
     let bulletPosition = (slider.value - (slider.max - slider.min) / 2) / slider.max; // bulletPosition ranges from -0.5 to +0.5
     bullet.style.left = `${bulletPosition * getSliderCSSWidth()}px`; // Sets the left position of the bullet
 }
 
-
-// 
+// Gets the width (defined in CSS) of the slider element
 function getSliderCSSWidth() {
     return (Number(getComputedStyle(slider).getPropertyValue('width').slice(0,-2)));
 }
 
+// Generates a fresh grid of divs based on the current slider value
 function generateDivs(){
     // Remove any child nodes that are alrd there
     while (sketchContainer.hasChildNodes()) {
@@ -44,7 +28,6 @@ function generateDivs(){
     sketchContainer.style.gridTemplateColumns = fractionString;
     sketchContainer.style.gridTemplateRows = fractionString;
 
-
     /// Create mini-divs
     for (let i = 0; i < totalSquares; i++){
         const div = document.createElement('div');
@@ -53,21 +36,24 @@ function generateDivs(){
     }
 }
 
+// Changes the color of the div to either black, gradual, rainbow or white(erase) based on the colorStatus
 function changeDivColor(e){
     if (colorStatus === "black" || colorStatus === "white") {
         e.target.style.backgroundColor = colorStatus;
     }
     else if (colorStatus === "gradual"){
-        console.log(e.target.style.backgroundColor);
+
+        // If the backgroundColor is an empty string / null / undefined (falsy value), make the cell 10% black.
         if (!e.target.style.backgroundColor){
             e.target.style.backgroundColor = "rgb(230, 230, 230)";
-            console.log(e.target.style.backgroundColor);
         }
+        // Else if the backgroundColor has an existing color, extract these r,g,b values and return them 10% blacker.
         else{
             let colorString = e.target.style.backgroundColor;
             let cutColorString = colorString.slice(colorString.indexOf("(") + 1 , colorString.indexOf(")"));
             let colorArray = cutColorString.split(", ");
-            console.log(colorArray);
+
+            // Function either subtracts 25 from an r,g,b value, OR, if value is too low, set it to 0 to avoid -ve numbers
             colorArray.forEach(function (colorVal, colorIndex){
                 if (Number(colorVal) > 30){
                     colorArray[colorIndex] = Number(colorVal) - 25;
@@ -75,10 +61,8 @@ function changeDivColor(e){
                 else{
                     colorArray[colorIndex] = 0;
                 }
-                console.log(colorVal);
             })
             e.target.style.backgroundColor = `rgb(${colorArray[0]}, ${colorArray[1]}, ${colorArray[2]})`;
-            console.log(e.target.style.backgroundColor);
         }
     }
     else if (colorStatus === "rainbow"){
@@ -90,38 +74,72 @@ function changeDivColor(e){
     }
 }
 
+// Updates the colorStatus value and updates button pressed appearance based on what button the user has pressed
 function defineColor(e){
     if (e.target.classList.contains('black')){
         colorStatus = "black";
+        black.classList.add('active');
+        gradual.classList.remove('active');
+        rainbow.classList.remove('active');
+        erase.classList.remove('active');
     }
     else if (e.target.classList.contains('gradual')){
         colorStatus = "gradual";
+        black.classList.remove('active');
+        gradual.classList.add('active');
+        rainbow.classList.remove('active');
+        erase.classList.remove('active');
     }
     else if (e.target.classList.contains('rainbow')){
         colorStatus = "rainbow";
+        black.classList.remove('active');
+        gradual.classList.remove('active');
+        rainbow.classList.add('active');
+        erase.classList.remove('active');
     }
     else if (e.target.classList.contains('erase')){
         colorStatus = "white";
+        black.classList.remove('active');
+        gradual.classList.remove('active');
+        rainbow.classList.remove('active');
+        erase.classList.add('active');
     }
 }
 
+// Define important elements with variables and set initial pen type to black
+const slider = document.querySelector(".slider");
+const bullet = document.querySelector(".bullet");
+const sketchContainer = document.querySelector(".sketch-container");
+const black = document.querySelector(".black");
+const gradual = document.querySelector(".gradual");
+const rainbow = document.querySelector(".rainbow");
+const erase = document.querySelector(".erase");
+const reset = document.querySelector(".reset");
+let colorStatus = "black";
+black.classList.add('active');
 
+// Sets the bullet value to the initial HTML slider value (35) when the page loads
+bullet.textContent = `${slider.value} x ${slider.value}`;
+
+// Sets the bullet value to the live slider value when user input is registered
+slider.addEventListener('input',showSliderValue)
+
+// Generates div grid when page loads the 1st time, and subsequently whenever the slider value is changed
 generateDivs();
-
 slider.addEventListener('input',generateDivs);
 
+// Updates the colorStatus value based on what button the user has pressed
 black.addEventListener('click',defineColor);
 gradual.addEventListener('click',defineColor);
 rainbow.addEventListener('click',defineColor);
 erase.addEventListener('click',defineColor);
 
+// Make cursor change to pointer when hovering over button
+black.style.cursor = "pointer";
+gradual.style.cursor = "pointer";
+rainbow.style.cursor = "pointer";
+erase.style.cursor = "pointer";
+reset.style.cursor = "pointer";
+
+// When reset button is pressed, resets the div grid by generating a fresh grid
 reset.addEventListener('click',generateDivs);
-
-// let miniDivLength = `${100/slider.value}%`;
-// Width of each mini div in pixels. Obtained with (sketch container width) / number of divs
-// let miniDivWidth = Number(getComputedStyle(sketchContainer).getPropertyValue('width').slice(0,-2)) / slider.value;
-// div.setAttribute('class','mini-div');
-// div.style.width = miniDivLength;
-// div.style.height = miniDivLength;
-// div.style.gridTemplateColumns = ""
-
